218aa1b414121eed7901831abbad9fc16f9e84ca
Favour Nwachukwu <fnwachukwu84@gmail.com>
2022-06-03 22:28:52 +0100

    FIX - Add correct file path for test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable quotes */
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./components/App";
 
-test('renders App', () => {
+test("renders App", () => {
   render(<App />);
 });
