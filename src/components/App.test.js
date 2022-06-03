7001491773af32da5058c44161cb8d3036fd7bd7
Favour Nwachukwu <fnwachukwu84@gmail.com>
2022-06-03 22:42:30 +0100

    Bug fix 3

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,18 @@
+/* eslint-disable import/order */
+/* eslint-disable comma-dangle */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable quotes */
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import Router from "react-router-dom";
 
 test("renders App", () => {
-  render(<App />);
+  // render(<App />);
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
 });
