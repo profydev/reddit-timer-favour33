88d32be847d6d6ff7a70ba6a11595f8e581c3145
Favour Nwachukwu <fnwachukwu84@gmail.com>
2022-06-03 22:52:56 +0100

    Bug fix 5

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,7 +5,7 @@
 /* eslint-disable quotes */
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "src/components/App.js";
+import App from "./src/components/App.js";
 
 test("renders App", () => {
   // render(<App />);
