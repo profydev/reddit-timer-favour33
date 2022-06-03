aaf0b6bdf61639e6747c6d32049c72949b3a27db
Favour Nwachukwu <fnwachukwu84@gmail.com>
2022-06-03 22:10:23 +0100

    Routes to naviagte for Home and Search Routes

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+/* eslint-disable quotes */
+// eslint-disable-next-line quotes
+import React from "react";
+// eslint-disable-next-line quotes
+import { Route, Routes } from "react-router-dom";
+
+import Search from "./Search/Search";
+import Home from "./Home/Home";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+// eslint-disable-next-line quotes
+import React from "react";
+
+const Search = () => {
+  return <div>Search</div>;
+};
+
+export default Search;
